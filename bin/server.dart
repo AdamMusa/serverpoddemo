5c48378cb97680ef930e0cffc97dc8bfce50229b
AdamMusa <adammusa2222@gmail.com>
2026-07-16 02:40:23 -0400

    Add bin/server.dart

--- bin/server.dart
+++ bin/server.dart
@@ -1,1 +1,15 @@
-import "dart:io";\n\nFuture<void> main(List<String> args) async {\n  final port = int.tryParse(Platform.environment["PORT"] ?? "") ?? 8080;\n  final server = await HttpServer.bind(InternetAddress.anyIPv4, port);\n  await for (final request in server) {\n    request.response.headers.contentType = ContentType.text;\n    if (request.uri.path == "/up") {\n      request.response.write("ok");\n    } else {\n      request.response.write("Serverpod on Alkimist");\n    }\n    await request.response.close();\n  }\n}\n
+import "dart:io";
+
+Future<void> main(List<String> args) async {
+  final port = int.tryParse(Platform.environment["PORT"] ?? "") ?? 8080;
+  final server = await HttpServer.bind(InternetAddress.anyIPv4, port);
+  await for (final request in server) {
+    request.response.headers.contentType = ContentType.text;
+    if (request.uri.path == "/up") {
+      request.response.write("ok");
+    } else {
+      request.response.write("Serverpod on Alkimist");
+    }
+    await request.response.close();
+  }
+}
